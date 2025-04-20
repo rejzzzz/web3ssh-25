2f5d182968b64bc3f501d4f11cbdbb3c2f4b12d8
rejzzzz <156344567+rejzzzz@users.noreply.github.com>
2025-04-20 17:08:47 +0530

    removed register button from 2024

--- src/components/components2024/HeroSection2024.jsx
+++ src/components/components2024/HeroSection2024.jsx
@@ -65,29 +65,6 @@ const HeroSection = () => {
 
       <div className="my-2">
         <div className="mt-10 lg:mt-10 flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 items-center justify-center">
-          <a href="/register">
-            <button className="w-64 transform hover:-translate-y-1 transition duration-400 relative inline-flex sm:h-16 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 pl-12 pr-8 py-4 sm:text-lg font-semibold text-white backdrop-blur-3xl text-lg">
-                Register Now
-                <svg
-                  width="32"
-                  height="32"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M10.75 8.75L14.25 12L10.75 15.25"
-                  />
-                </svg>
-              </span>
-            </button>
-          </a>
           <a href="https://web3ssh.devfolio.co/" target="_blank">
             <button className="w-full h-full bg-[#3770FF] flex items-center justify-center p-3 text-lg rounded-full">
               <span className="p-0.5 inline-flex items-center">
