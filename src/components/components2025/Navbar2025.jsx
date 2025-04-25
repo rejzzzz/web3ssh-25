221fa4f276d543789012aab852169c54c33efc72
rejzzzz <156344567+rejzzzz@users.noreply.github.com>
2025-04-25 20:40:43 +0530

    ethereum added

--- src/components/components2025/Navbar2025.jsx
+++ src/components/components2025/Navbar2025.jsx
@@ -195,7 +195,7 @@ export default function Navbar() {
             </li>
           </ul>
 
-          <a href="/brochure" className="inline-block">
+          <a href="/brochure/2025" className="inline-block">
             <button
               type="button"
               className="flex items-center justify-center w-40 px-5 py-2.5 text-xs lg:text-sm font-medium text-white transition-all duration-300 bg-gradient-to-br from-purple-600 to-blue-500 rounded-lg hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
