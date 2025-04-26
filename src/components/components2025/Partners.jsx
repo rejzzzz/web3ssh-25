525ac4909bcd566f3ae9b2b406bb55e76dae6358
rejzzzz <156344567+rejzzzz@users.noreply.github.com>
2025-04-26 20:16:50 +0530

    fix eth

--- src/components/components2025/Partners.jsx
+++ src/components/components2025/Partners.jsx
@@ -1,10 +1,17 @@
 'use client';
 import { AnimatedTooltip } from '../ui/animated-tooltip';
 import j3dai_logo from './../../../public/partners2025/J3d.ai_Logo_White.png';
-import { Ethereum } from './sponsors/Ethereum';
+import Ethereum from './../../../public/partners2025/eth-logo-white.svg';
 
 export default function Partners() {
-  const sponsors = ["https://ethereum.org/en/"];
+  const sponsors = [
+    {
+      name: 'Ethereum',
+      logo: Ethereum,
+      link: 'https://ethereum.org/en/',
+      type: 'Title Sponsor',
+    },
+  ];
   const partners = [
     {
       name: 'J3D.ai',
@@ -19,16 +26,28 @@ export default function Partners() {
       <div className="w-full lg:pb-0">
         <div className="w-full h-fit grid grid-cols-1 gap-14 sm:grid-cols-2 sm:gap-5 md:grid-cols-2 text-gray-400">
           {/* Sponsors Section */}
-                <div className="w-full h-fit space-y-6">
-                <h1 className="text-white text-4xl text-center mb-5">Sponsors</h1>
-                <div className="flex w-full h-fit justify-center items-center flex-wrap">
-                  <div className="text-white text-2xl p-4">
-                <a href={sponsors[0]} target="_blank" rel="noopener noreferrer">
-                  <Ethereum />
+          <div className="w-full h-fit space-y-6">
+            <h1 className="text-white text-4xl text-center mb-5">Sponsors</h1>
+            <div className="flex w-full h-fit justify-center items-center flex-wrap">
+              {sponsors.map((partner, index) => (
+                <a
+                  key={index}
+                  className="p-4 flex flex-col items-center justify-center transform transition duration-300 hover:scale-105 hover:translate-y-2 cursor-pointer"
+                  target="_blank"
+                  href={partner.link}
+                >
+                  <div className="h-[120px] flex justify-center items-center">
+                    <img
+                      src={partner.logo.src}
+                      alt={partner.name}
+                      className="w-[160px] h-[auto] sm:h-[50px] md:w-[160px] md:h-[auto]"
+                    />
+                  </div>
+                  <div className="text-white text-xl">{partner.type}</div>
                 </a>
-                </div>
-                </div>
-                </div>
+              ))}
+            </div>
+          </div>
 
           {/* Partners Section */}
           <div className="w-full h-fit space-y-6">
