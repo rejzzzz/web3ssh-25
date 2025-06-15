497331f19297aa57a570f7b45e796234737a87c5
rejzzzz <156344567+rejzzzz@users.noreply.github.com>
2025-06-15 21:30:15 +0530

    updated deadline

--- src/components/components2025/HeroSection2025.jsx
+++ src/components/components2025/HeroSection2025.jsx
@@ -117,7 +117,7 @@ export default function HeroSection() {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            Registration closes June 15, 2025
+            Registration closes June 22, 2025
           </div>
         </div>
       </div>
